6caa8a013bdb9dff741556baa92953072d5199c0
Nicolas Figueroa <nicolasfigb@gmail.com>
2022-12-14 00:12:09 -0500

    Finished email validation and desktop styling

--- app.js
+++ app.js
@@ -4,15 +4,24 @@ let pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 let button = document.querySelector(".submit-btn");
 
 let validate = () => {
-  console.log();
   if (email.value === "") {
     error.style.display = "block";
+    email.classList.remove("success-input");
     email.classList.add("form-error");
+    error.style.color = "var(--light-red)";
     error.innerHTML = "Whoops! It looks like you forgot to add your email";
-  }
-  if (!pattern.test(email.value)) {
+  } else if (!pattern.test(email.value)) {
+    email.classList.remove("success-input");
+    email.classList.add("form-error");
     error.style.display = "block";
+    error.style.color = "var(--light-red)";
     error.innerHTML = "Please provide a valid email address";
+  } else if (pattern.test(email.value)) {
+    email.classList.remove("form-error");
+    email.classList.add("success-input");
+    error.style.display = "block";
+    error.style.color = "var(--green)";
+    error.innerHTML = "Thank you for your support!";
   }
 };
 
