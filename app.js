6f5238c121d668fcf3f42aec7354b5812a461b00
Nicolas Figueroa <nicolasfigb@gmail.com>
2022-12-13 23:46:29 -0500

    Basic custom error messages

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 let email = document.querySelector(".email");
 let error = document.querySelector(".error");
-let pattern = "^w+@[a-zA-Z_]+?.[a-zA-Z]{2,3}$";
-
+let pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 let button = document.querySelector(".submit-btn");
 
 let validate = () => {
-  console.log(typeof email.value);
-  console.log(email.value.match(pattern));
+  console.log();
   if (email.value === "") {
     error.style.display = "block";
     email.classList.add("form-error");
     error.innerHTML = "Whoops! It looks like you forgot to add your email";
+  }
+  if (!pattern.test(email.value)) {
+    error.style.display = "block";
+    error.innerHTML = "Please provide a valid email address";
   }
 };
 
